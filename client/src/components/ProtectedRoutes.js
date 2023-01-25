9099e37549e52610e7cb94cb187c7a0c388ace4b
Michael W Jarrett <67067893+mjay88@users.noreply.github.com>
2023-01-24 18:10:45 -0600

    styling seperate side nav for mobile, added heroicons

--- client/src/components/ProtectedRoutes.js
+++ client/src/components/ProtectedRoutes.js
@@ -5,36 +5,43 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import SideBar from "./SideBar";
 import AccordionCopy from "./AccordionCopy";
+import MobileSideNav from "./MobileSideNav";
 {
-  /**we pass the ProtectedRoutes component as the element of <Route> wrapping the protected routes. Since we need to preserve the Layout, I placed that inside of the render for the ProtectedRoutes component.  */
+	/**we pass the ProtectedRoutes component as the element of <Route> wrapping the protected routes. Since we need to preserve the Layout, I placed that inside of the render for the ProtectedRoutes component.  */
 }
 
 const ProtectedRoutes = () => {
-  const { user } = useAuth();
+	const { user } = useAuth();
 
-  console.log("Check user in Private: ", user);
+	console.log("Check user in Private: ", user);
 
-  if (!user) {
-    return <Navigate to="/Landing" />;
-  }
-  return (
-    <div>
-      <Header />
+	if (!user) {
+		return <Navigate to="/Landing" />;
+	}
+	return (
+		<div>
+			<Header />
 
-      {/**An <Outlet> should be used in parent route elements to render their child route elements. This allows nested UI to show up when child routes are rendered. If the parent route matched exactly, it will render a child index route or nothing if there is no index route. Essentially we replaced our <Layout/> component with <ProtectedRoutes />, we pass the ProtectedRoutes component as the element of <Route> wrapping the protected routes. Since we need to preserve the layout, we took it from our old <Layout/> component and placed that inside of the render for the ProtectedRoutes component.*/}
+			{/**An <Outlet> should be used in parent route elements to render their child route elements. This allows nested UI to show up when child routes are rendered. If the parent route matched exactly, it will render a child index route or nothing if there is no index route. Essentially we replaced our <Layout/> component with <ProtectedRoutes />, we pass the ProtectedRoutes component as the element of <Route> wrapping the protected routes. Since we need to preserve the layout, we took it from our old <Layout/> component and placed that inside of the render for the ProtectedRoutes component.*/}
 
-      <div className="flex flex-column">
-        <div className="basis-1/4">
-          <AccordionCopy />
-        </div>
+			<div className="flex flex-column">
+				<div className="basis-1/4">
+					<div className="hidden md:block">
+						<AccordionCopy />
+					</div>
 
-        <div className="flex justify-center basis-3/4">
-          <div className=""></div>
-          <Outlet />
-        </div>
-      </div>
-    </div>
-  );
+					<div className="lg:hidden">
+						<MobileSideNav />
+					</div>
+				</div>
+
+				<div className="flex justify-center basis-3/4">
+					<div className=""></div>
+					<Outlet />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ProtectedRoutes;
